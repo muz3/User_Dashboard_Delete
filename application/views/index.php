307d54a37db8eb9c2f6738b1300fd73188813fc3
Rui Wang <wangr3@gmail.com>
2015-05-20 22:19:57 -0700

    index.php

--- application/views/index.php
+++ application/views/index.php
@@ -32,7 +32,7 @@
 
 	<nav class="navbar navbar-default navbar-static-top">
 		<div class="container">
-			<h3 class="navbar-text nav-left">Test App</h3>
+			<h3 class="navbar-text nav-left">User DashBoard</h3>
 			<p class="navbar-text nav-left"><a href="#" class="navbar-link">Home</a></p>
 			<p class="navbar-text pull-right"><a href="/signin" class="navbar-link">Sign in</a></p>
 		</div>
@@ -44,7 +44,7 @@
 
 		
 		<div class="container">
-			<h3>Welcome to the Test</h3>
+			<h3>Welcome to User DashBoard</h3>
 			<p>We're going to build a cool application using a MVC framework! The application was built with the Rui Wang!</p>
 			<p><a class = "btn btn-large btn-info" href="#">Start</a></p>	
 		</div>
